09c46117a8516a0280644b4a1a4d2d6ed2ebb0ba
yu-k-m <youkm0806@sookmyung.ac.kr>
2025-01-31 01:52:59 +0900

    docs: update leet-183.sql

--- 유경민/week5/SQL/leet-183.sql
+++ 유경민/week5/SQL/leet-183.sql
@@ -1,4 +1,4 @@
 SELECT c.Name AS Customers
 FROM Customers c
-LEFT JOIN Orders o ON c.id = o.CustomerId
+LEFT JOIN Orders o ON c.id = o.CustomerId  #null을 나타내야하므로 NOT IN (X) INNER JOIN(X)은 매칭되는 rows만 나옴 
 WHERE o.CustomerId IS NULL;
