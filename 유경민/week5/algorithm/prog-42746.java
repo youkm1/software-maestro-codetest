2a6dbb511d17204835eebb4b9ae57643643f32ab
yu-k-m <youkm0806@sookmyung.ac.kr>
2025-02-01 20:54:25 +0900

    docs:Update prog-42746.java

--- 유경민/week5/algorithm/prog-42746.java
+++ 유경민/week5/algorithm/prog-42746.java
@@ -9,7 +9,8 @@ class Solution {
         Arrays.sort(strNum, (a,b) -> (b+a).compareTo(a+b));
         
         if (strNum[0].equals("0")) return "0";
-        
+
+        //String.join도 내부적으로 StringBuilder을 쓰므로 성능은 비슷합니다
         StringBuilder result = new StringBuilder();
         for (String num:strNum) {
             result.append(num);
